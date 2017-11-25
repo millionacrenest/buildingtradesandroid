5236dd113fdf79815b8816686c924698e0d1ad83
millionacrenest <millionacrenest@gmail.com>
2017-11-25 13:22:39 -0800

    removed experiment part two

--- app/src/main/java/com/allisonmcentire/buildingtradesandroid/MainActivity.java
+++ app/src/main/java/com/allisonmcentire/buildingtradesandroid/MainActivity.java
@@ -6,47 +6,23 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-import com.allisonmcentire.buildingtradesandroid.Adapter.MyAdapter;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView list;
-    RecyclerView.LayoutManager layoutManager;
-    List<News> items = new ArrayList<>();
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_menu);
+        setContentView(R.layout.activity_main);
 
-        list = (RecyclerView)findViewById(R.id.recycler);
-        list.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-        list.setLayoutManager(layoutManager);
 
-        setData();
     }
 
-    private void setData() {
-        for(int i=0;i<20;i++)
-        {
-            if(i%2==0)
-            {
-                News item = new News("This is item "+(i+1),true);
-                items.add(item);
-            }
-            else
-            {
-                News item = new News("This is item "+(i+1),false);
-                items.add(item);
-            }
-        }
 
-        MyAdapter adapter = new MyAdapter(items);
-        list.setAdapter(adapter);
-    }
 }
